0962f60a373999ef6c2af57a1e32147929a1c5be
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-10 11:15:00 +0200

    add css file

--- app.py
+++ app.py
@@ -6,4 +6,16 @@ from flask import Flask, request, render_template, redirect, url_for
 # create flask object
 app = Flask(__name__, template_folder="templates")
 
+todos = [{"task": "todo 1", "done": False},
+         {"task": "todo 2", "done": False},
+         {"task": "todo 3", "done": False}]
 
+@app.route("/")  # This decorator is needed to map the URL to the function
+def index():
+    """
+    index function to render index.html
+    """
+    return render_template("index.html", todos=todos)
+
+if __name__ == "__main__":
+    app.run(debug=True)
