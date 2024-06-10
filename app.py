ce95c7d30fed0d283a11023fca91bd07faec4ec8
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-10 10:28:03 +0200

    update app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+# TODO APP
+# -*- coding: utf-8 -*-
+# import flask module and template.
+from flask import Flask, request, render_template, redirect, url_for
+
+# create flask object
+app = Flask(__name__, template_folder="templates")
+
+
